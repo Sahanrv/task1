cc61b7cb4931ea52abcd4fb9e76c92f0db42ddc3
Ravindu <treinetic.macbook.001@gmail.com>
2019-07-29 18:29:56 +0530

    spin it

---
+++ slot/Spin It/Spin It/controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spin It
+//
+//  Created by Treinetic Macbook 001 on 7/29/19.
+//  Copyright © 2019 Treinetic Macbook 001. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
